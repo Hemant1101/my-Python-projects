4f5680ff6bc2d6647578927859b13a6dd5ccb38e
Hemant Kumar <63533985+Hemant1101@users.noreply.github.com>
2021-02-02 16:00:42 +0530

    Update listcomprehensions.py

--- listcomprehensions.py
+++ listcomprehensions.py
@@ -1,3 +1,7 @@
+# author:Hemant1101
+# lang:  Python
+# topic: list comprehensions
+
 l: [int] = list(x for x in range(1, 11))     # list:[type] = list(expression for item in iterable)
 print(l)
 n = [x for x in range(1, 11)]                # list = [expression for item in iterable]
@@ -6,6 +10,7 @@ m = [x for x in range(1, 20) if(x%2==0)]      # list:[type] = list(expression fo
 print(m)
 a = [x if x % 3 == 0 else -1 for x in range(1, 20)]  # list:[type]=list(expression if stmt else stmt for i in iterable)
 print(a)
+
 coordinates = [[x,y,z] for x in range(4) for y in range(4) for z in range(4) if((x+y+z)<4)]
 print(coordinates)
 x = int(input())
